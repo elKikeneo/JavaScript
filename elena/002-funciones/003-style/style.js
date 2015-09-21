2d6655378bc467545a00779c26482d29661fb153
Rauliomagno <raguionbajo46@hotmail.com>
2015-09-21 11:45:02 +0200

    clases

---
+++ elena/002-funciones/003-style/style.js
@@ -0,0 +1,19 @@
+//var global
+var titulo;
+var texto;
+
+//Hacemos los selectores una vez cargado el documento, y lo hacemos con var globales para que el resto de funciones tengan acceso a ellos
+function init(){
+   titulo=document.getElementsByTagName("h1")[0];
+   texto=document.getElementById("texto");
+}
+
+function ponerEstiloAzul(){
+    var colorAzul = "#2d437f";
+    titulo.style.color = colorAzul;
+    texto.style.color = colorAzul;
+    texto.innerHTML = "El color utilizado es "+colorAzul;
+}
+
+function ponerEstiloVerde(){
+}
