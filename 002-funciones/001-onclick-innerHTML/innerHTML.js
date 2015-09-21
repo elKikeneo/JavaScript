a884c11f76fae5cd13b73ff759c71f9b412f7a9f
Rauliomagno <raguionbajo46@hotmail.com>
2015-09-21 11:39:37 +0200

    clases

---
+++ 002-funciones/001-onclick-innerHTML/innerHTML.js
@@ -0,0 +1,14 @@
+
+var parrafo=document.getElementById("texto");
+
+function saludar(){
+    parrafo.innerHTML = "Hola";
+}
+
+function despedir(){
+    parrafo.innerHTML = "Adios";
+}
+
+function reset(){
+    parrafo.innerHTML = "";
+}
---
+++ 002-funciones/004-link-href/theme.js
@@ -0,0 +1,16 @@
+
+var link=document.getElementById("theme");
+
+function themeGris(){
+    link.href = "css/gris.css";
+}
+function themeAzul(){
+    link.href = "css/azul.css";
+}
+function themeReload(){
+    link.href = "";
+}
+
+
+
+
