1834262f4b5263de4ade0a9e0e3dd02e52e9e9ab
kikeneo <luismedca@hotmail.com>
2015-10-12 11:46:37 +0200

    clases

--- 002-funciones/012-onResize/onResize.js
+++ 002-funciones/012-onResize/onResize.js
@@ -6,9 +6,11 @@ function getMedidas(){
     document.getElementById("height").innerHTML = window.innerHeight;
     
     if(window.innerWidth>600){
-        
+        document.getElementsByTagName("main")[0].style.background="#777";
+        //document.getElementsByTagName("link")[0].href="style.css";
     }else{
-        
+        document.getElementsByTagName("main")[0].style.background="#222";
+        //document.getElementsByTagName("link")[0].href="styleMovil.css";
     }
     
 }
